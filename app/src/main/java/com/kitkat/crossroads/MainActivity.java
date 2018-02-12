d9150c3f04e36c55ace08f0155213b3151aae336
craigmartin2013 <craigmartin2013@yahoo.co.uk>
2018-02-12 13:40:06 +0000

    Created functionality for registration and a mock, registration page

--- app/src/main/java/com/kitkat/crossroads/MainActivity.java
+++ app/src/main/java/com/kitkat/crossroads/MainActivity.java
@@ -1,13 +1,105 @@
 package com.kitkat.crossroads;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignUp;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+    private void registerUser()
+    {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        // email is too short
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please Enter An Email Address", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // password to short
+        if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please Enter A Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //if validation is ok, show progress bar
+        progressDialog.setMessage("Register User...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this, "Registered Sucessfully", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "Could Not Register. Please Try Again", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewSignUp = (TextView) findViewById(R.id.textViewSignIn);
+
+
+        buttonRegister.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                registerUser();
+            }
+        });
+
+        textViewSignUp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
     }
 }
