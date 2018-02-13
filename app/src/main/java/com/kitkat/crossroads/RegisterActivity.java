9e7ae4ddcdd2739784f08447b199d1eff3d085a0
craigmartin97 <craigmartin2013@yahoo.co.uk>
2018-02-13 11:15:57 +0000

    Changed validation, must tick checkbox t&c before creating an account and enter a password greater than 6 chars

--- app/src/main/java/com/kitkat/crossroads/RegisterActivity.java
+++ app/src/main/java/com/kitkat/crossroads/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,63 +17,66 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class RegisterActivity extends AppCompatActivity
-{
+public class RegisterActivity extends AppCompatActivity {
 
     // WHERE ON THE DEVELOP BRANCH
 
     private Button buttonRegister;
     private EditText editTextEmail;
     private EditText editTextPassword;
+    private CheckBox checkBox;
     //private TextView textViewSignUp;
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
 
-    private void registerUser()
-    {
+    private void registerUser() {
         String email = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
 
         // email is too short
-        if(TextUtils.isEmpty(email))
-        {
+        if (TextUtils.isEmpty(email)) {
             Toast.makeText(this, "Please Enter An Email Address", Toast.LENGTH_SHORT).show();
             return;
         }
 
         // password to short
-        if(TextUtils.isEmpty(password))
-        {
+        if (TextUtils.isEmpty(password)) {
             Toast.makeText(this, "Please Enter A Password", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        //if validation is ok, show progress bar
-        progressDialog.setMessage("Register User...");
-        progressDialog.show();
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters", Toast.LENGTH_SHORT);
+        }
 
-        firebaseAuth.createUserWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
-                {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task)
-                    {
-                        if(task.isSuccessful())
-                        {
-                            Toast.makeText(RegisterActivity.this, "Registered Sucessfully", Toast.LENGTH_SHORT).show();
-                        }
-                        else
-                        {
-                            Toast.makeText(RegisterActivity.this, "Could Not Register. Please Try Again", Toast.LENGTH_SHORT).show();
-                        }
+        if (checkBox.isChecked()) {
+            //if validation is ok, show progress bar
+            progressDialog.setMessage("Register User...");
+            progressDialog.show();
 
-                    }
-                });
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                progressDialog.dismiss();
+                                Toast.makeText(RegisterActivity.this, "Registered Sucessfully", Toast.LENGTH_SHORT).show();
+                            } else if (!task.isSuccessful() && password.length() < 6){
+                                progressDialog.dismiss();
+                                Toast.makeText(RegisterActivity.this, "Could Not Register. Passwords much be at least 6 characters ", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                progressDialog.dismiss();
+                                Toast.makeText(RegisterActivity.this, "Could Not Register. Please Check Your Details Again And Try Again", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
@@ -82,14 +86,13 @@ public class RegisterActivity extends AppCompatActivity
         buttonRegister = (Button) findViewById(R.id.buttonRegister);
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
         //textViewSignUp = (TextView) findViewById(R.id.textViewSignIn);
 
 
-        buttonRegister.setOnClickListener(new View.OnClickListener()
-        {
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 registerUser();
             }
         });
