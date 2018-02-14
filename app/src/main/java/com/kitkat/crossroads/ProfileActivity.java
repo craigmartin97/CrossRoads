e4357a97de17c1099d1ea24b95ed9f2cef465ab8
craigmartin1997 <craigmartin2013@yahoo.co.uk>
2018-02-14 13:59:34 +0000

    Added the profile page, and functionality. Auto login after register

---
+++ app/src/main/java/com/kitkat/crossroads/ProfileActivity.java
@@ -0,0 +1,50 @@
+package com.kitkat.crossroads;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private TextView textViewUserEmail;
+    private Button buttonLogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() == null)
+        {
+            finish();
+            startActivity(new Intent(this,LoginActivity.class));
+        }
+
+        FirebaseUser user = auth.getCurrentUser();
+
+
+        textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+        textViewUserEmail.setText("Welcome " + user.getEmail());
+
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                finish();
+                startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
