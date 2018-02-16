6bd387af7a2f3d1b085be7202936cec6f2cb6d8d
craigmartin1997 <xpgm7crn$jpant>
2018-02-16 09:27:12 +0000

    Fixed the profile display page

--- app/src/main/java/com/kitkat/crossroads/ViewProfileActivity.java
+++ app/src/main/java/com/kitkat/crossroads/ViewProfileActivity.java
@@ -1,10 +1,16 @@
 package com.kitkat.crossroads;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,40 +21,53 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+
 public class ViewProfileActivity extends AppCompatActivity {
+    private static final String TAG = "ViewProfileActivity";
 
-    private TextView textViewName;
-    private TextView textViewPostalAddress;
-    private TextView textViewDateOfBirth;
-    private TextView textViewPhoneNumber;
-    private TextView textViewEditProfile;
-
-    private FirebaseDatabase firebaseDatabase;
-    private FirebaseAuth auth;
-    private FirebaseAuth.AuthStateListener authStateListener;
-    private DatabaseReference databaseReference;
+    //add Firebase Database stuff
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
     private String userID;
 
-
+    private ListView mListView;
+    private TextView textViewName;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
 
+        mListView = (ListView) findViewById(R.id.listView);
         textViewName = (TextView) findViewById(R.id.textViewName);
-        textViewPostalAddress = (TextView) findViewById(R.id.textViewPostalAddress);
-        textViewDateOfBirth = (TextView) findViewById(R.id.textViewDateOfBirth);
-        textViewPhoneNumber = (TextView) findViewById(R.id.textViewPhoneNumber);
-        textViewEditProfile = (TextView) findViewById(R.id.textViewEditProfile);
 
-        auth = FirebaseAuth.getInstance();
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference();
-        FirebaseUser user = auth.getCurrentUser();
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference().child("users");
+        FirebaseUser user = mAuth.getCurrentUser();
         userID = user.getUid();
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+            }
+        };
+
+        myRef.child("users").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 showData(dataSnapshot);
@@ -60,29 +79,53 @@ public class ViewProfileActivity extends AppCompatActivity {
             }
         });
 
-
-        textViewEditProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(ViewProfileActivity.this, CreateProfileActivity.class));
-            }
-        });
     }
 
-    private void showData(DataSnapshot dataSnapshot)
-    {
-        for (DataSnapshot ds : dataSnapshot.getChildren())
-        {
-            UserDetails userDetails = new UserDetails();
-            userDetails.setName(ds.child(userID).getValue(UserDetails.class).getName());
-            userDetails.setAddress(ds.child(userID).getValue(UserDetails.class).getAddress());
-            userDetails.setDateOfBirth(ds.child(userID).getValue(UserDetails.class).getDateOfBirth());
-            userDetails.setPhoneNumber(ds.child(userID).getValue(UserDetails.class).getPhoneNumber());
+    private void showData(DataSnapshot dataSnapshot) {
+        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+            UserDetails uInfo = new UserDetails();
+            uInfo.setAddress(ds.child(userID).getValue(UserDetails.class).getAddress()); //set the email
+            uInfo.setDateOfBirth(ds.child(userID).getValue(UserDetails.class).getDateOfBirth());
+            uInfo.setName(ds.child(userID).getValue(UserDetails.class).getName()); //set the name
+            uInfo.setPhoneNumber(ds.child(userID).getValue(UserDetails.class).getPhoneNumber());
 
-            textViewName.setText(userDetails.getName());
-            textViewPostalAddress.setText(userDetails.getAddress());
-            textViewDateOfBirth.setText(userDetails.getDateOfBirth());
-            textViewPhoneNumber.setText(userDetails.getPhoneNumber());
+
+            //display all the information
+            Log.d(TAG, "showData: name: " + uInfo.getName());
+            Log.d(TAG, "showData: address: " + uInfo.getAddress());
+            Log.d(TAG, "showData: phoneNumber: " + uInfo.getPhoneNumber());
+            Log.d(TAG, "showData: dateOfBirth: " + uInfo.getDateOfBirth());
+
+            ArrayList<String> array = new ArrayList<>();
+            array.add(uInfo.getName());
+            array.add(uInfo.getAddress());
+            array.add(uInfo.getPhoneNumber());
+            array.add(uInfo.getDateOfBirth());
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, array);
+            mListView.setAdapter(adapter);
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    /**
+     * customizable toast
+     *
+     * @param message
+     */
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
