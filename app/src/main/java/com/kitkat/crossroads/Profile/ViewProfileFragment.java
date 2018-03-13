62cb208ae4ac5b17d66e4c05c5c162db99b2d25a
craigmartin97 <craigmartin2013@yahoo.co.uk>
2018-03-13 10:06:11 +0000

    Created Circle Transformation For Circular Images, And Resized - Craig

--- app/src/main/java/com/kitkat/crossroads/Profile/ViewProfileFragment.java
+++ app/src/main/java/com/kitkat/crossroads/Profile/ViewProfileFragment.java
@@ -22,6 +22,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.kitkat.crossroads.CircleTransformation;
 import com.kitkat.crossroads.R;
 import com.squareup.picasso.Picasso;
 
@@ -163,7 +164,7 @@ public class ViewProfileFragment extends Fragment {
                 addressTwo.setText(address2);
                 town.setText(usersTown);
                 postCode.setText(postalCode);
-                Picasso.get().load(profileImage).into(profileImageUri);
+                Picasso.get().load(profileImage).resize(300,300).transform(new CircleTransformation()).into(profileImageUri);
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
