5e3886222bfc340a7f3c464cf72d48dc1b416736
craigmartin97 <craigmartin2013@yahoo.co.uk>
2018-03-14 13:40:53 +0000

    Created connection to new upload image fragment - Craig

--- app/src/main/java/com/kitkat/crossroads/UploadImageFragment.java
+++ app/src/main/java/com/kitkat/crossroads/UploadImageFragment.java
@@ -91,8 +91,6 @@ public class UploadImageFragment extends Fragment
             mListener = (OnFragmentInteractionListener) context;
         } else
         {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
         }
     }
 
