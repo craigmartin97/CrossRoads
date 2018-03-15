6cf59f456ae8193c3d7e0b1004a5d8a95372f644
craigmartin1997 <q5031372@live.tees.ac.uk>
2018-03-15 11:02:39 +0000

    Included MyCustomAdapter on JobsBids - Craig

--- app/src/main/java/com/kitkat/crossroads/JobBidsFragment.java
+++ app/src/main/java/com/kitkat/crossroads/JobBidsFragment.java
@@ -1,22 +1,30 @@
 package com.kitkat.crossroads;
 
 import android.content.Context;
+import android.database.DataSetObserver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.kitkat.crossroads.Jobs.JobInformation;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 
 /**
@@ -27,7 +35,7 @@ import java.util.ArrayList;
  * Use the {@link JobBidsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class JobBidsFragment extends Fragment
+public class JobBidsFragment extends Fragment implements SearchView.OnQueryTextListener
 {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -49,7 +57,7 @@ public class JobBidsFragment extends Fragment
     private ArrayList<String> userBidId = new ArrayList<String>();
     private ArrayList<String> userJobId = new ArrayList<String>();
 
-    private MyJobsFragment.MyCustomAdapter mAdapter;
+    private JobBidsFragment.MyCustomAdapter mAdapter;
 
     private ArrayList<JobInformation> jobList = new ArrayList<>();
 
@@ -101,7 +109,26 @@ public class JobBidsFragment extends Fragment
     {
         View view = inflater.inflate(R.layout.fragment_job_bids, container, false);
 
+        jobListView = view.findViewById(R.id.jobListView1);
 
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference();
+
+        databaseReference.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+
+            }
+        });
 
 
         return view;
@@ -135,6 +162,22 @@ public class JobBidsFragment extends Fragment
         mListener = null;
     }
 
+    @Override
+    public boolean onQueryTextSubmit(String query)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText)
+    {
+        String text = newText;
+        mAdapter.filter(text);
+
+        return false;
+    }
+
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -145,9 +188,170 @@ public class JobBidsFragment extends Fragment
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener
+    interface OnFragmentInteractionListener
     {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+
+    public class MyCustomAdapter extends BaseAdapter
+    {
+
+        private ArrayList<JobInformation> mData = new ArrayList();
+        private ArrayList<JobInformation> mDataOrig = new ArrayList();
+
+        private LayoutInflater mInflater;
+
+        public MyCustomAdapter()
+        {
+
+            if (isAdded())
+            {
+                mInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            }
+        }
+
+        public void addItem(final JobInformation item)
+        {
+            mData.add(item);
+            mDataOrig.add(item);
+        }
+
+
+        public void addArray(final ArrayList<JobInformation> j)
+        {
+            mData = j;
+            mDataOrig = j;
+        }
+
+
+        @Override
+        public void registerDataSetObserver(DataSetObserver observer)
+        {
+
+        }
+
+        @Override
+        public void unregisterDataSetObserver(DataSetObserver observer)
+        {
+
+        }
+
+        @Override
+        public int getCount()
+        {
+            return mData.size();
+        }
+
+        @Override
+        public Object getItem(int position)
+        {
+            return mData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position)
+        {
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds()
+        {
+            return false;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent)
+        {
+            System.out.println("getView " + position + " " + convertView);
+            JobBidsFragment.MyCustomAdapter.GroupViewHolder holder;
+            if (convertView == null)
+            {
+                convertView = mInflater.inflate(R.layout.job_info_list, null);
+                holder = new JobBidsFragment.MyCustomAdapter.GroupViewHolder();
+                holder.textViewName = convertView.findViewById(R.id.textName);
+                holder.textViewFrom = convertView.findViewById(R.id.textFrom);
+                holder.textViewTo = convertView.findViewById(R.id.textTo);
+                holder.detailsButton = convertView.findViewById(R.id.detailsButton);
+                convertView.setTag(holder);
+            } else
+            {
+                holder = (JobBidsFragment.MyCustomAdapter.GroupViewHolder) convertView.getTag();
+            }
+
+            holder.textViewName.setText(mData.get(position).getAdvertName());
+            holder.textViewFrom.setText(mData.get(position).getColL1());
+            holder.textViewTo.setText(mData.get(position).getDelL1());
+            holder.detailsButton.setOnClickListener(new View.OnClickListener()
+            {
+
+                @Override
+                public void onClick(View v)
+                {
+
+                    JobDetailsFragment jobDetailsFragment = new JobDetailsFragment();
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("Job", mData.get(position));
+                    jobDetailsFragment.setArguments(bundle);
+                    FragmentManager fragmentManager = getFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.content, jobDetailsFragment).commit();
+                }
+            });
+            return convertView;
+        }
+
+        @Override
+        public boolean areAllItemsEnabled()
+        {
+            return false;
+        }
+
+        @Override
+        public boolean isEmpty()
+        {
+            return false;
+        }
+
+        public class GroupViewHolder
+        {
+            public TextView textViewName;
+            public TextView textViewFrom;
+            public TextView textViewTo;
+            public Button detailsButton;
+        }
+
+        public void filter(String charText)
+        {
+
+            ArrayList<JobInformation> jobs = new ArrayList<JobInformation>();
+            ArrayList<JobInformation> jA = new ArrayList<JobInformation>();
+            charText = charText.toLowerCase(Locale.getDefault());
+
+            if (charText.length() == 0)
+            {
+                mData = mDataOrig;
+            } else
+            {
+
+                for (JobInformation j : mDataOrig)
+                {
+                    if (j.getWholeString().toLowerCase(Locale.getDefault()).contains(charText))
+                    {
+                        jobs.add(j);
+                        jA.add(j);
+                    } else
+                    {
+                        jA.add(j);
+                    }
+                }
+                mData.clear();
+                mData = jobs;
+                mDataOrig = jA;
+            }
+
+            notifyDataSetChanged();
+        }
     }
 }
