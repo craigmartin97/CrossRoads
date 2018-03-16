3edd8a2b378699bd160c13e960dfeb04bfc6273a
s6042911 <omcburney25@gmail.com>
2018-03-16 16:06:28 +0000

    --Oliver-- --16/3/18--

--- app/src/main/java/com/kitkat/crossroads/FindAJobFragment.java
+++ app/src/main/java/com/kitkat/crossroads/FindAJobFragment.java
@@ -2,6 +2,7 @@ package com.kitkat.crossroads;
 
 import android.content.Context;
 import android.database.DataSetObserver;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,10 +11,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +30,8 @@ import com.google.firebase.database.ValueEventListener;
 import com.kitkat.crossroads.Jobs.JobInformation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 
@@ -63,6 +69,8 @@ public class FindAJobFragment extends Fragment implements SearchView.OnQueryText
 
     private ListView jobListView;
 
+    private Spinner sortBySpinner;
+    private Button filterButton;
     private SearchView jobSearch;
 
 
@@ -110,6 +118,85 @@ public class FindAJobFragment extends Fragment implements SearchView.OnQueryText
 
         jobListView = (ListView) view.findViewById(R.id.jobListView1);
 
+        sortBySpinner = (Spinner) view.findViewById(R.id.sortBySpinner);
+
+        String[] sortBy = new String[]{
+                "Sort By",
+                "Name",
+                "Collection From",
+                "Delivery To",
+                "Collection Date",
+                "Size"
+        };
+
+        final List<String> sortByList = new ArrayList<>(Arrays.asList(sortBy));
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, sortByList){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sortBySpinner.setAdapter(adapter1);
+
+        sortBySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                // If user change the default selection
+                // First item is disable and it is used for hint
+                if(position > 0){
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getActivity(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        filterButton = (Button) view.findViewById(R.id.filterButton);
+
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(filterButton.getTag().equals("#2bbc9b"))
+                {
+                    TextView filterName = (TextView) view.findViewById(R.id.filterName);
+                    filterName.setVisibility(view.GONE);
+                }
+            }
+        });
+
+
 
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
@@ -124,7 +211,7 @@ public class FindAJobFragment extends Fragment implements SearchView.OnQueryText
 
                 Iterable<DataSnapshot> jobListSnapShot = jobReference.getChildren();
 
-                mAdapter = new FindAJobFragment.MyCustomAdapter();
+                mAdapter = new MyCustomAdapter();
 
                 for (DataSnapshot ds : jobListSnapShot)
                 {
@@ -321,7 +408,7 @@ public class FindAJobFragment extends Fragment implements SearchView.OnQueryText
                     bundle.putSerializable("Job", mData.get(position));
                     jobDetailsFragment.setArguments(bundle);
                     FragmentManager fragmentManager = getFragmentManager();
-                    fragmentManager.beginTransaction().replace(R.id.content, jobDetailsFragment).commit();
+                    fragmentManager.beginTransaction().replace(R.id.content, jobDetailsFragment).addToBackStack("tag").commit();
                 }
             });
             return convertView;
