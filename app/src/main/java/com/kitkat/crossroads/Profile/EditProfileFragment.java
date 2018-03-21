b91fd019e0c8b68b2438a3ed4d74bd3e09b2d7e2
craigmartin1997 <xpgm7crn$jpant>
2018-03-21 11:04:46 +0000

    My adverts now displays all types of bid - Liam & Craig

--- app/src/main/java/com/kitkat/crossroads/Profile/EditProfileFragment.java
+++ app/src/main/java/com/kitkat/crossroads/Profile/EditProfileFragment.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
--- app/src/main/java/com/kitkat/crossroads/UploadImageFragment.java
+++ app/src/main/java/com/kitkat/crossroads/UploadImageFragment.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -45,9 +46,12 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
 import java.io.IOException;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -240,12 +244,65 @@ public class UploadImageFragment extends Fragment
             progressDialog.setMessage("Displaying Image...");
             progressDialog.show();
 
-            imageUri = data.getData();
-            Picasso.get().load(imageUri).into(profileImage);
+            //imageUri = data.getData();
+            //Picasso.get().load(imageUri).into(profileImage);
+
+
             final Uri uri = data.getData();
-            Picasso.get().load(uri).into(profileImage);
+
+
+            File file = new File(uri.toString());
+            file.getAbsolutePath();
+
             progressDialog.dismiss();
+            try
+            {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+                modifyOrientation(bitmap,file.getAbsolutePath());
+                profileImage.setImageBitmap(bitmap);
+            }
+            catch(IOException e)
+            {
+                e.getStackTrace();
+            }
         }
+    }
+
+    public static Bitmap modifyOrientation(Bitmap bitmap, String image_absolute_path) throws IOException {
+        ExifInterface ei = new ExifInterface(image_absolute_path);
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotate(bitmap, 90);
+
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotate(bitmap, 180);
+
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotate(bitmap, 270);
+
+            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+                return flip(bitmap, true, false);
+
+            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+                return flip(bitmap, false, true);
+
+            default:
+                return bitmap;
+        }
+    }
+
+    public static Bitmap rotate(Bitmap bitmap, float degrees) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degrees);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    public static Bitmap flip(Bitmap bitmap, boolean horizontal, boolean vertical) {
+        Matrix matrix = new Matrix();
+        matrix.preScale(horizontal ? -1 : 1, vertical ? -1 : 1);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
