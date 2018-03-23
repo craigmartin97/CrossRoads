17191fd666fd3d0ff2787ce4422b787bb66314e0
craigmartin1997 <xpgm7crn$jpant>
2018-03-23 13:12:09 +0000

    Mereg

--- app/src/main/java/com/kitkat/crossroads/Jobs/MyJobsFragment.java
+++ app/src/main/java/com/kitkat/crossroads/Jobs/MyJobsFragment.java
@@ -43,14 +43,14 @@ import java.util.Locale;
 public class MyJobsFragment extends Fragment implements SearchView.OnQueryTextListener
 {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+//    // TODO: Rename parameter arguments, choose names that match
+//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+//    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -99,9 +99,9 @@ public class MyJobsFragment extends Fragment implements SearchView.OnQueryTextLi
     {
         MyJobsFragment fragment = new MyJobsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
         return fragment;
     }
 
@@ -110,11 +110,11 @@ public class MyJobsFragment extends Fragment implements SearchView.OnQueryTextLi
     {
         super.onCreate(savedInstanceState);
         tabTag = "Active";
-        if (getArguments() != null)
-        {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        if (getArguments() != null)
+//        {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
 
     @Override
