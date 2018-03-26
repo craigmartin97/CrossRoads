dc7f009c0e68526a35d13a3ba881d2c2f7b51771
Craig <q5031372@live.tees.ac.uk>
2018-03-26 22:09:26 +0100

    Added an edit bit text field - Craig

--- app/src/main/java/com/kitkat/crossroads/Jobs/BidDetailsFragment.java
+++ app/src/main/java/com/kitkat/crossroads/Jobs/BidDetailsFragment.java
@@ -245,45 +245,4 @@ public class BidDetailsFragment extends Fragment
 
         listHashMap3.put(list3.get(0), jobInformation);
     }
-
-    /**
-     * Setting the Lists Max heights
-     *
-     * @param listView
-     * @param group
-     */
-//    private void setListViewHeight(ExpandableListView listView,
-//                                   int group) {
-//        ExpandableListAdapter listAdapter = (ExpandableListAdapter) listView.getExpandableListAdapter();
-//        int totalHeight = 0;
-//        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(),
-//                View.MeasureSpec.EXACTLY);
-//        for (int i = 0; i < listAdapter.getGroupCount(); i++) {
-//            View groupItem = listAdapter.getGroupView(i, false, null, listView);
-//            groupItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
-//
-//            totalHeight += groupItem.getMeasuredHeight();
-//
-//            if (((listView.isGroupExpanded(i)) && (i != group))
-//                    || ((!listView.isGroupExpanded(i)) && (i == group))) {
-//                for (int j = 0; j < listAdapter.getChildrenCount(i); j++) {
-//                    View listItem = listAdapter.getChildView(i, j, false, null,
-//                            listView);
-//                    listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
-//
-//                    totalHeight += listItem.getMeasuredHeight() + 5;
-//
-//                }
-//            }
-//        }
-//
-//        ViewGroup.LayoutParams params = listView.getLayoutParams();
-//        int height = totalHeight
-//                + (listView.getDividerHeight() * (listAdapter.getGroupCount() - 1));
-//        if (height < 10)
-//            height = 200;
-//        params.height = height;
-//        listView.setLayoutParams(params);
-//        listView.requestLayout();
-//    }
 }
