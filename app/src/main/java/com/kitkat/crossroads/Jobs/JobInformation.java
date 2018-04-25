7d948ac4fd8526986f97ee448b3a659febd51578
q5031372 <q5031372@live.tees.ac.uk>
2018-04-25 15:54:06 +0100

    Finished javadocs on JobInformation - Craig

--- app/src/main/java/com/kitkat/crossroads/Jobs/JobInformation.java
+++ app/src/main/java/com/kitkat/crossroads/Jobs/JobInformation.java
@@ -6,8 +6,7 @@ import java.io.Serializable;
  * Creates a new jobInformation object that stores and creates a new job.
  * Jobs must have name, description, size, type and address information.
  */
-public class JobInformation implements Serializable
-{
+public class JobInformation implements Serializable {
 
     /**
      * All strings to store the jobs information
@@ -20,8 +19,7 @@ public class JobInformation implements Serializable
     /**
      * Blank constructor, needed for Serialization
      */
-    public JobInformation()
-    {
+    public JobInformation() {
 
     }
 
@@ -47,8 +45,7 @@ public class JobInformation implements Serializable
      * @param jobStatus         String: If the job is Pending, Active, Complete or Inactive
      * @param jobImage          String: URL address of the Image for the job
      */
-    public JobInformation(String advertName, String advertDescription, String jobSize, String jobType, String posterID, String courierID, String collectionDate, String collectionTime, String colL1, String colL2, String colTown, String colPostcode, String delL1, String delL2, String delTown, String delPostcode, String jobStatus, String jobImage)
-    {
+    public JobInformation(String advertName, String advertDescription, String jobSize, String jobType, String posterID, String courierID, String collectionDate, String collectionTime, String colL1, String colL2, String colTown, String colPostcode, String delL1, String delL2, String delTown, String delPostcode, String jobStatus, String jobImage) {
         this.advertName = advertName;
         this.advertDescription = advertDescription;
         this.jobSize = jobSize;
@@ -70,122 +67,217 @@ public class JobInformation implements Serializable
     }
 
     /**
-     * Set the jobId
+     * Set the jobId, when method is called the jobId variable is overridden
+     * Id is usually numbers and letters combined that are assigned to a job.
      *
-     * @param jobID
+     * @param jobID String: Id of job
      */
-    public void setJobID(String jobID)
-    {
+    public void setJobID(String jobID) {
         this.jobID = jobID;
     }
 
     /**
-     * Get the courierId
+     * Get the courierId, can gain access to the courierId of the object
+     * Courier Id is number an letters that are assigned to a user, who is the courier
+     * for the job
      *
      * @return String: Couriers Id
      */
-    public String getCourierID()
-    {
+    public String getCourierID() {
         return courierID;
     }
 
     /**
-     * Get the JobId
+     * Get the JobId, can gain access to the courierId of the object
+     * Id is usually numbers and letters combined that are assigned to a job.
      *
      * @return String: Job Id
      */
-    public String getJobID()
-    {
+    public String getJobID() {
         return jobID;
     }
 
-    public String getAdvertName()
-    {
+    /**
+     * Get the adverts name, can gain access to the advert name of the object
+     * Advert name, gives a brief description of what the job is about.
+     *
+     * @return String: Advert name, the adverts name
+     */
+    public String getAdvertName() {
         return advertName;
     }
 
-    public String getAdvertDescription()
-    {
+    /**
+     * Get the advert description, can gain access to the advert description of the object.
+     * The advert description tells the user more about the job.
+     *
+     * @return String: Advert Description, the description of the advert
+     */
+    public String getAdvertDescription() {
         return advertDescription;
     }
 
-    public String getJobSize()
-    {
+    /**
+     * Get the Job Size, can gain access to the jobSize of the object.
+     * The job size can either be small, medium or large. The user can select
+     * these from a preset menu.
+     *
+     * @return String: Job Size, the size of the job
+     */
+    public String getJobSize() {
         return jobSize;
     }
 
-    public String getJobType()
-    {
+    /**
+     * Get the JobType, can gain access to the jobType of the object.
+     * The job type can either be a single or multiple items. The user can select these
+     * from a preset list.
+     *
+     * @return String: Job Type, type of job
+     */
+    public String getJobType() {
         return jobType;
     }
 
-    public String getPosterID()
-    {
+    /**
+     * Get the posterId, can gain access to the posterId of the object.
+     * The poster id is the person who posted the job advert.
+     *
+     * @return String: Poster Id, user who posted job
+     */
+    public String getPosterID() {
         return posterID;
     }
 
-    public String getCollectionDate()
-    {
+    /**
+     * Get the collection date, can gain access to the collectionDate of the object.
+     * The date that the collection of the job is to take place
+     *
+     * @return String: Collection Date, date of collection
+     */
+    public String getCollectionDate() {
         return collectionDate;
     }
 
-    public String getCollectionTime()
-    {
+    /**
+     * Get the collection time, can gain access to the collecitonTime of the object.
+     * The time that the collection of the job is to take place.
+     *
+     * @return String: Collection Time, time of collection
+     */
+    public String getCollectionTime() {
         return collectionTime;
     }
 
-    public String getColL1()
-    {
+    /**
+     * Get the Collection Address Line One, can gain access to the collection address line
+     * The collection address line one that the job is to be collected from.
+     *
+     * @return String: Collection Address Line One, first part of address
+     */
+    public String getColL1() {
         return colL1;
     }
 
-    public String getColL2()
-    {
+    /**
+     * Get the collection address line two, can gain access to the collection address line
+     * The collection address line two that the job is to be collected from.
+     *
+     * @return
+     */
+    public String getColL2() {
         return colL2;
     }
 
-    public String getColTown()
-    {
+    /**
+     * Get the collection town, can gain access to the collection town.
+     * The collection town is the town that the job is to be collected from
+     *
+     * @return String: Collection town, town to be collected from
+     */
+    public String getColTown() {
         return colTown;
     }
 
-    public String getColPostcode()
-    {
+    /**
+     * Get the collection postcode, can gain access to the collection postcode.
+     * The collection postcode is the postcode the job is to be collected from
+     *
+     * @return String: Collection postcode, postal address code
+     */
+    public String getColPostcode() {
         return colPostcode;
     }
 
-    public String getDelL1()
-    {
+
+    /**
+     * Get the delivery address line one, can gain access to the delivery address line one.
+     * Delivery address line one, is the first part of where the job needs to be taken too.
+     *
+     * @return String: Delivery address line one, address line to be delivered too
+     */
+    public String getDelL1() {
         return delL1;
     }
 
-    public String getDelL2()
-    {
+    /**
+     * Get the delivery address line two, can gain access to the delivery address line two.
+     * Delivery address line two is the second part of the address where the job needs
+     * to be taken too.
+     *
+     * @return String: Delivery address line two, address line two where the job is delivered too
+     */
+    public String getDelL2() {
         return delL2;
     }
 
-    public String getDelTown()
-    {
+    /**
+     * Get the delivery town, can gain access to the delivery address town.
+     * Delivery address town where the job is being taken too.
+     *
+     * @return String: Delivery town, the town where the job is being taken too.
+     */
+    public String getDelTown() {
         return delTown;
     }
 
-    public String getDelPostcode()
-    {
+    /**
+     * Get the delivery postcode, can gain access to the delivery postcode.
+     * Delivery postcode is the postcode where the job is being delivery too.
+     *
+     * @return String: DeliveryPostcode, postcode of delivery address
+     */
+    public String getDelPostcode() {
         return delPostcode;
     }
 
-    public String getJobStatus()
-    {
+    /**
+     * Get the JobStatus, can gain access to the job status.
+     * Job status is the progress that the job is at. It can either be Pending, Active, Complete or Inactive
+     *
+     * @return String: Job Status, state that the job is in
+     */
+    public String getJobStatus() {
         return jobStatus;
     }
 
-    public String getJobImage()
-    {
+    /**
+     * Get the job image, can gain access to the job image.
+     * Job Image, is the image associated with the job. Can be displayed on pages
+     *
+     * @return String: URL address of the image
+     */
+    public String getJobImage() {
         return jobImage;
     }
 
-    public String getWholeString()
-    {
+    /**
+     * Concatenates the wholeObject together to be printed
+     * Used so we can see each element that has been successfully added to the Job Information class
+     *
+     * @return String: WholeString, all objects
+     */
+    public String getWholeString() {
         return advertName + advertDescription + jobSize + jobType + collectionDate + collectionTime + colL1 + colL2 + colTown + colPostcode + delL1 + delL2 + delTown + delPostcode;
     }
 }
