ba7b451cfeb3f6c7d9cc3e5346ff4d3f961f954e
Craig <q5031372@live.tees.ac.uk>
2018-04-29 21:37:05 +0100

    JavaDoc for CrossRoads completed - Craig

--- app/src/main/java/com/kitkat/crossroads/MainActivity/CrossRoads.java
+++ app/src/main/java/com/kitkat/crossroads/MainActivity/CrossRoads.java
@@ -5,9 +5,10 @@ import android.app.Application;
 import com.google.firebase.database.FirebaseDatabase;
 
 /**
- * Created by q5031372 on 24/04/18.
+ * Every time the app is ran this class is called to enable data persistence while the users
+ * device is offline. This will ensure that data is cached locally to the phone for a better user
+ * experience.
  */
-
 public class CrossRoads extends Application
 {
     @Override
