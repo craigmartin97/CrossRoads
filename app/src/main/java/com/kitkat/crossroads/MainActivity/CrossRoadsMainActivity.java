bda8851cdb0acffa8abbb8207193848286dfe313
craigmartin1997 <xpgm7crn$jpant>
2018-05-04 09:39:22 +0100

    fixed errors with nav - Craig

--- app/src/main/java/com/kitkat/crossroads/MainActivity/CrossRoadsMainActivity.java
+++ app/src/main/java/com/kitkat/crossroads/MainActivity/CrossRoadsMainActivity.java
@@ -32,7 +32,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
