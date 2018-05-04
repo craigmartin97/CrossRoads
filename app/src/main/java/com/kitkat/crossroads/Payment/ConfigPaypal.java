7c7ddd1353d82b7eba27e63b5630dfb25b1f8ec7
craigmartin1997 <xpgm7crn$jpant>
2018-05-04 11:10:25 +0100

    Added comments to payments - Craig

--- app/src/main/java/com/kitkat/crossroads/Payment/ConfigPaypal.java
+++ app/src/main/java/com/kitkat/crossroads/Payment/ConfigPaypal.java
@@ -1,5 +1,8 @@
 package com.kitkat.crossroads.Payment;
 
+/**
+ * Stores the reference to our paypal account for payment
+ */
 public class ConfigPaypal
 {
     public static final String PAYPAL_CLIENT_ID = "AbWv4xX8FPeAO0wjONbngqceP_Kis8LKlpbhnGRSDgT3HchqsO5jVown-d1g9fby-YTNbZhLkwLyN4cA";
